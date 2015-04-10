b9d561fad3a64e3cedb5be196702dda92e353e36
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-11 00:28:46 +0200

    PieChart first draft

---
+++ Example/PieChartExample/PieChart.ios.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var React = require('React');
+var NativeModules = require('NativeModules');
+var ReactIOSViewAttributes = require('ReactIOSViewAttributes');
+var StyleSheet = require('StyleSheet');
+var createReactIOSNativeComponentClass = require('createReactIOSNativeComponentClass');
+var PropTypes = require('ReactPropTypes');
+var NativeMethodsMixin = require('NativeMethodsMixin');
+var flattenStyle = require('flattenStyle');
+var merge = require('merge');
+
+var PieChart = React.createClass({
+  mixins: [NativeMethodsMixin],
+  viewConfig: {
+    uiViewClassName: 'UIView',
+    validAttributes: ReactIOSViewAttributes.UIView
+  },
+  render: function () {
+    var style = flattenStyle([styles.base, this.props.style]);
+    var nativeProps = merge(this.props, {
+      style
+    });
+    return <RNPieChart {...nativeProps}/>
+  }
+});
+
+var RNPieChart = createReactIOSNativeComponentClass({
+  validAttributes: merge(ReactIOSViewAttributes.UIView, {}),
+  uiViewClassName: 'RNPieChart'
+});
+
+var styles = StyleSheet.create({
+  base: {}
+});
+
+module.exports = PieChart;
